504c2745e2505f654d044e33a506f0cc1105206c
lidan <l1Dan@Hotmail.com>
2016-12-06 23:30:40 +0800

    设置 UITabbarItem 全局颜色

--- WeiBo/MainViewController.swift
+++ WeiBo/MainViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class MainViewController: UITabBarController {
-
+    private lazy var composeButton: UIButton = {
+        let composeButton = UIButton()
+        composeButton.setBackgroundImage(UIImage(named: "tabbar_compose_button"), for: .normal)
+        composeButton.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), for: .highlighted)
+        composeButton.setImage(UIImage(named: "tabbar_compose_icon_add"), for: .normal)
+        composeButton.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), for: .highlighted)
+        composeButton.sizeToFit()
+        return composeButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // 设置全局的 UITabBarItem 颜色
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.lightGray], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.orange], for: .selected)
+        
+        composeButton.center = CGPoint(x: tabBar.center.x, y: tabBar.bounds.size.height * 0.5)
+        composeButton.addTarget(self, action: #selector(clickComposeButton(_:)), for: .touchUpInside)
+        tabBar.addSubview(composeButton)
+    }
+    
+    // MARK: - Compose button click
+    @objc private func clickComposeButton(_ sender: UIButton) {
+        print("clickComposeButton")
     }
     
     override func didReceiveMemoryWarning() {

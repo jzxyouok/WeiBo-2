becd8f6cf8e28e560df60bd92ad080bcd26b497c
lidan <l1Dan@Hotmail.com>
2016-12-10 02:38:55 +0800

    添加欢迎界面

--- WeiBo/Frontend/Welcome/WelcomeViewController.swift
+++ WeiBo/Frontend/Welcome/WelcomeViewController.swift
@@ -7,29 +7,40 @@
 //
 
 import UIKit
+import Kingfisher
 
 class WelcomeViewController: UIViewController {
+    @IBOutlet weak var avatorImageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var avatorImageView: UIImageView!
+    @IBOutlet weak var avatorLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        avatorImageView.layer.cornerRadius = 39
+        avatorImageView.clipsToBounds = true
+        
+        let account = Account.shared
+        // 设置欢迎语
+        avatorLabel.text = account.name + " " + avatorLabel.text!
+        // 下载图片
+        let url = URL(string: account.avatarLarge)
+        avatorImageView.kf.setImage(with: url, placeholder: UIImage(named: "avator_default"))
+        // 动画
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: [], animations: {
+            self.avatorImageViewTopConstraint.constant = 160
+            self.view.layoutIfNeeded()
+        }) { (isFinished) in
+            if isFinished {
+                // 设置根控制器
+                let homeVc = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+                UIApplication.shared.keyWindow?.rootViewController = homeVc
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
